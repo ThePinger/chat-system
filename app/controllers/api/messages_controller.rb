143699b41b5b2ec1a6af9cbd50bad42251182df5
Omar Emad <omaremadsabry@gmail.com>
2024-05-12 21:14:34 +0300

    [*] Updated messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -4,12 +4,7 @@ class Api::MessagesController < ApplicationController
 
     # GET /api/applications/:application_token/chats/:chat_num/messages
     def index
-        application = Application.find_by(token: params[:application_token])
-        if application.nil?
-            return render json: { error: "application not found" }, status: :not_found
-        end
-
-        chat = application.chats.find_by(num: params[:chat_num])
+        chat = Chat.get_chat_by_token_and_num(params[:application_token], params[:chat_num])
         if chat.nil?
             return render json: { error: "chat not found" }, status: :not_found
         end
@@ -20,32 +15,33 @@ class Api::MessagesController < ApplicationController
 
     # POST /api/applications/:application_token/chats/:chat_num/messages
     def create
-        application = Application.find_by(token: params[:application_token])
-        if application.nil?
-            return render json: { error: "application not found" }, status: :not_found
-        end
-
-        chat = application.chats.find_by(num: params[:chat_num])
+        chat = Chat.get_chat_by_token_and_num(params[:application_token], params[:chat_num])
         if chat.nil?
             return render json: { error: "chat not found" }, status: :not_found
         end
 
         message = Message.new(chat_id: chat.id, content: params[:content])
-        if message.save
-            render json: message, except: EXCLUDED_FIELDS, status: :created
+        if message.valid?
+            # Generate the message num within the chat
+            message.num = Chat.increment_messages_count_by_token_and_num(params[:application_token], params[:chat_num])
+            if message.num.nil?
+                return render json: { error: "failed to create message" }, status: :internal_server_error
+            end
+
+            # Push the chat to the RabbitMQ
+            publisher = Publisher.new("messages")
+            publisher.publish(message)
+            publisher.close
+
+            return render json: message, except: EXCLUDED_FIELDS, status: :created
         else
-            render json: message.errors, status: :bad_request
+            return render json: message.errors, status: :bad_request
         end
     end
 
     # GET /api/applications/:application_token/chats/:chat_num/messages/:num
     def show
-        application = Application.find_by(token: params[:application_token])
-        if application.nil?
-            return render json: { error: "application not found" }, status: :not_found
-        end
-
-        chat = application.chats.find_by(num: params[:chat_num])
+        chat = Chat.get_chat_by_token_and_num(params[:application_token], params[:chat_num])
         if chat.nil?
             return render json: { error: "chat not found" }, status: :not_found
         end
@@ -55,5 +51,25 @@ class Api::MessagesController < ApplicationController
             return render json: { error: "message not found" }, status: :not_found
         end
         render json: message, except: EXCLUDED_FIELDS, status: :ok
+    end
+
+    # PUT /api/applications/:application_token/chats/:chat_num/messages/:num
+    def update
+        chat = Chat.get_chat_by_token_and_num(params[:application_token], params[:chat_num])
+        if chat.nil?
+            return render json: { error: "chat not found" }, status: :not_found
+        end
+
+        message = chat.messages.find_by(num: params[:num])
+        if message.nil?
+            return render json: { error: "message not found" }, status: :not_found
+        end
+
+        message.content = params[:content]
+        if message.save
+            return render json: message, except: EXCLUDED_FIELDS, status: :ok
+        else
+            return render json: message.errors, status: :bad_request
+        end
     end
 end
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,8 +2,60 @@ class Chat < ApplicationRecord
     belongs_to :application
     has_many :messages, dependent: :destroy
 
-    before_create do
-        # Generate the chat num within the application
-        self.num = self.application.chats.count + 1
+    after_create do
+        Rails.cache.write("chat_#{self.application.token}_#{self.num}", self.to_json)
+        Rails.cache.write("chat_#{self.application.token}_#{self.num}_messages_count", 0)
+    end
+
+    after_update do
+        Rails.cache.write("chat_#{self.application.token}_#{self.num}", self.to_json)
+    end
+
+    after_destroy do
+        Rails.cache.write("chat_#{self.application.token}_#{self.num}", self.to_json)
+    end
+
+    def self.get_chat_by_token_and_num(application_token, num)
+        key = "chat_#{application_token}_#{num}"
+        if Rails.cache.exist?(key)
+            chat = Rails.cache.read(key)
+            chat = JSON.parse(chat, object_class: Chat) if chat.present?
+        else
+            application = Application.get_application_by_token(application_token)
+            chat = Chat.find_by(application_id: application.id, num: num) if application.present?
+            Rails.cache.write(key, chat.to_json) if chat.present?
+        end
+        return chat
+    end
+
+    def self.increment_messages_count_by_token_and_num(application_token, num)
+        key = "chat_#{application_token}_#{num}_messages_count"
+
+        # Lock the cache key using redlock
+        lock_manager = Redlock::Client.new(["redis://127.0.0.1:6379"], {
+            retry_count: 3,
+            retry_delay: 200
+        })
+
+        begin
+            messages_count = lock_manager.lock!("#{key}_lock", 5000) do
+                if Rails.cache.exist?(key)
+                    count = Rails.cache.read(key)
+                    count = count.to_i + 1
+                    Rails.cache.write(key, count)
+                else
+                    application = Application.get_application_by_token(application_token)
+                    chat = Chat.find_by(application_id: application.id, num: num)
+                    count = chat.messages.count + 1
+                    Rails.cache.write(key, count)
+                end
+                count
+            end
+            return messages_count
+        rescue Redlock::LockError
+            # error handling
+            print "Error: LockError"
+            return nil
+        end
     end
 end
